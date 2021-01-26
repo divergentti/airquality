fa2340c0797f2eb1ceb1df6a189808db7314482f
Jari Hiltunen <65418152+divergentti@users.noreply.github.com>
2021-01-26 17:13:36 +0200

    Update MHZ19B_AS.py

--- esp32/esp32-mhz19-ili9341-touchscreen/MHZ19B_AS.py
+++ esp32/esp32-mhz19-ili9341-touchscreen/MHZ19B_AS.py
@@ -37,8 +37,8 @@ class MHZ19bCO2:
         self.crc_errors = 0
         self.range_errors = 0
         self.measuring_range = '0_5000'  # default
-        self.preheat_time = 10   # shall be 180 or more, during testing you can use these values
-        self.read_interval = 10  # shall be 120 or more
+        self.preheat_time = 189   # shall be 180 or more, during testing you can use these values
+        self.read_interval = 120  # shall be 120 or more
         self.READ_COMMAND = bytearray(b'\xFF\x01\x86\x00\x00\x00\x00\x00\x79')
         self.CALIBRATE_ZEROPOINT = bytearray(b'\xFF\x01\x87\x00\x00\x00\x00\x00\x78')
         self.CALIBRATE_SPAN = bytearray(b'\xFF\x01\x88\x07\xD0\x00\x00\x00\xA0')
